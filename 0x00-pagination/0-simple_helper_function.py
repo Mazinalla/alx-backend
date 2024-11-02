454347719151ee7f99be5ee5a460a1923fb686bb
mazinalla <mazinelsadig8@gmail.com>
2024-11-02 03:56:40 +0300

    first implement of pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
 '''
-Function that take two int argu and return tuple containing start and an end index
+Function that take two argu and return tuple containing start and an end index
 params - (page, page_size)
 return - tuple()
 '''
 
 
 def index_range(page, page_size):
-    
+
     '''
-    Function that take two int argu and return tuple containing start and an end index
+    Function that take two argu and return tuple containing start, end index
     '''
     if page == 0:
         return (page - 1, page_size)
     else:
         return ((page - 1) * page_size, page_size * page)
-
